942643f849cddc2e2ad176f8d2672e0af4571fec
BKB <blainekb94@gmail.com>
2022-10-04 05:39:25 -0500

    declared variables in script

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const apiKey = "47d7ba8c1a57c412777ddcb40635e9c5"
+let searchBtn = $("btn-search")
+let citySearchContainer = $("city-search-container")
+let searchInput = $("input-search")
+let city = ""
+let cityNameDisplay = $("city-name")
+let currentDate = moment().format("LL")
+console.log(currentDate);
+const date = $("date")
+const forecastContainer = $("forecast-container")
+const forecastDisplay = $("forecast")
+const cityList = $("city-list")
