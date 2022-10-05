2ee272693f3f3309727ca687dcddd31c65df47d5
BKB <blainekb94@gmail.com>
2022-10-05 18:15:21 -0500

    added api fetch function to get city coordiantes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,36 @@ const apiKey = "47d7ba8c1a57c412777ddcb40635e9c5"
 let searchBtn = document.querySelector("#btn-search")
 let citySearchContainer = document.getElementById("city-search-container")
 let searchInput = document.getElementById("input-search")
-let city = ""
+const city = "Austin"
 let cityNameDisplay = document.getElementById("city-name")
-let currentDate = moment().format("LL")
-console.log(currentDate);
+// let currentDate = moment().format("LL")
 const cityList = document.getElementById("city-list")
 const date = document.getElementById("date")
 const forecastContainer = document.getElementById("forecast-container")
 const forecastDisplay = document.getElementById("forecast")
 
-const cityCoordinates = function(city) {
-  console.log("hi");
+
+const cityCoordinates = function() {
+  var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",&limit=1&appid=" + apiKey;
+  console.log(apiUrl);
+  
+  fetch(apiUrl)
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    }
+  })
 }
 
-console.log(searchBtn);
+cityCoordinates();
 
-searchBtn.addEventListener('click', function(event) {
-  event.preventDefault()
-  city = searchInput.value.trim()
-  console.log(city);
+
+// searchBtn.addEventListener('click', function(event) {
+//   event.preventDefault()
+//   city = searchInput.value.trim()
+//   console.log(city);
   
-  if (city) {
-    cityCoordinates(city)
-  }
-})
+//   if (city) {
+//     cityCoordinates(city)
+//   }
+// })
