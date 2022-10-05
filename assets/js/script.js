8909d530225b1718d5762a20d16121e38f16642f
BKB <blainekb94@gmail.com>
2022-10-05 18:22:11 -0500

    fixed api fetch function to parse json and return lat/long coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const apiKey = "47d7ba8c1a57c412777ddcb40635e9c5"
 let searchBtn = document.querySelector("#btn-search")
 let citySearchContainer = document.getElementById("city-search-container")
 let searchInput = document.getElementById("input-search")
-const city = "Austin"
+let city = "Austin"
 let cityNameDisplay = document.getElementById("city-name")
 // let currentDate = moment().format("LL")
 const cityList = document.getElementById("city-list")
@@ -20,6 +20,13 @@ const cityCoordinates = function() {
     if (response.ok) {
       return response.json()
     }
+  })
+  .then(data => {
+    let latitude = data[0].lat
+    let longitude = data[0].lon
+
+    console.log(latitude);
+    console.log(longitude);
   })
 }
 
