ec390265ef3ab0c95643f6fa5f8bd7309f84f3d2
BKB <blainekb94@gmail.com>
2022-10-07 00:59:47 -0500

    added in comments and did some minor styling, finally fixed 2nd API fetch but ran low on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ const forecastContainer = document.getElementById("forecast-container")
 const forecastDisplay = document.getElementById("forecast")
 const cityHistory = JSON.parse(localStorage.getItem("city-name")) || []
 
-
+// Function that takes the user input for a city and fetches geo coordinates from the API and returns the longitude and latitude
 const cityCoordinates = function(city) {
   var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",&limit=1&appid=" + apiKey;
   console.log(geoApiUrl);
@@ -30,9 +30,9 @@ const cityCoordinates = function(city) {
   })
 }
 
-
+// Function that fetches weather data for the user selected city by inserting the geo coordinates as parameters
 const showWeatherInfo = function(latitude, longitude, city) {
-  var weatherApiUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
+  var weatherApiUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
   console.log(weatherApiUrl);
   console.log(city);
 
@@ -47,11 +47,13 @@ const showWeatherInfo = function(latitude, longitude, city) {
   })
 }
 
+// Function that updates local storage with the name of the city
 function updateLocalStorage() {
   cityHistory.push(city)
   localStorage.setItem("city-name", JSON.stringify(cityHistory))
 }
 
+// Event listener for the search button that doesn't refresh the page and calls the appropriate functions once clicked
 searchBtn.addEventListener('click', function(event) {
   event.preventDefault()
   city = searchInput.value.trim()
@@ -59,5 +61,7 @@ searchBtn.addEventListener('click', function(event) {
   
   if (city) {
     cityCoordinates(city)
+    updateLocalStorage()
   }
 })
+
